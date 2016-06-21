30c1c22201a72afd530a12e28e7333896eed9634
hong <ghdrjsgml@gmail.com>
2016-06-21 16:43:49 +0900

    init heroku

--- app.js
+++ app.js
@@ -126,5 +126,5 @@ app.post('/deletelecture',routes.deletelecture);
 app.post('/writereply',routes.writereply);
 app.post('/showallreply',routes.showallreply);
 http.createServer(app).listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+   console.log('listening on', http.address().port);
 });
