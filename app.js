f4e7a1206e9157e6f202e85bcd16f02ff6aabd61
hong <ghdrjsgml@gmail.com>
2016-06-21 18:07:21 +0900

    Procfile

--- app.js
+++ app.js
@@ -125,7 +125,8 @@ app.post('/deletelecture',routes.deletelecture);
 
 app.post('/writereply',routes.writereply);
 app.post('/showallreply',routes.showallreply);
-http.createServer(app).listen(process.env.PORT || 3000, function(){
-  	  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+http.createServer(app).listen(app.get('port'), function(){
+  	   console.log('Express server listening on port ' + app.get('port'));
+
 
 });
