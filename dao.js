573de3ca2afdeb76aa5c2dae25e5d2ff0b57bab1
hong <ghdrjsgml@gmail.com>
2016-06-24 06:48:50 +0900

    init heroku

--- dao.js
+++ dao.js
@@ -1,7 +1,7 @@
 ﻿
 var Mongolian = require('mongolian')
 if(process.env.PORT){
-	var db = new Mongolian('mongodb://hongdon:rjsgml8911@ds021751.mlab.com:21751/pickthecube')
+	var db = new Mongolian(process.env.MONGODB_URI)
 	
 }else{
 	var	server = new Mongolian
