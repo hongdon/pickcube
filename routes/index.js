e97a7ba34f14f47854888668cd988c87831dd5e1
hong <ghdrjsgml@gmail.com>
2016-06-24 11:12:09 +0900

    init heroku

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ exports.join = function(req,res){
 	var data ={nickname : req.body.nickname	,email: req.body.email,password:req.body.password}
 	//var resultquery=dao.joinformcheck(data);
 	
-	dao.joinformchect(data).on('joinfinish',function(err,result){
+	dao.joinformcheck(data).on('joinfinish',function(err,result){
 		if(err){
 			console.log('joinfinisherror'+err)
 			
