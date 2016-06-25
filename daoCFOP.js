c3ec0b510d571885b142cb9e4dfcc03503813325
hong <ghdrjsgml@gmail.com>
2016-06-25 18:24:30 +0900

    init heroku

--- daoCFOP.js
+++ daoCFOP.js
@@ -129,7 +129,7 @@ var daoCFOP = module.exports={
 			//console.log(data.cubeObj); 
 			var totalpage;
 			console.log(data.cubeObj);
-			f2ldic.find({cubeobj:{$all: data.cubeObj}},function(err,results){
+			f2ldic.find({cubeobj:{$elemMatch: data.cubeObj}},function(err,results){
 				console.log(results);
 			})
 			
