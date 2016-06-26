c64c94a36e3734371590f61b7ac2c1d6e6916b6c
hong <ghdrjsgml@gmail.com>
2016-06-26 11:13:29 +0900

    init heroku

--- daoCFOP.js
+++ daoCFOP.js
@@ -165,7 +165,7 @@ var daoCFOP = module.exports={
 			var cubeObj1 = require('mongoose').Schema.Types.Array
 			var cubeObj2 = new cubeObj1(data.cubeObj)
 					console.log(cubeObj2);
-			f2ldic.find({cubeobj:{$all: {$all: {$all :cubeObj2}}}},function(err,results){
+			f2ldic.find({cubeobj:cubeObj2.path},function(err,results){
 				console.log("없냐??")
 				console.log(results)
 				
