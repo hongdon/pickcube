697459a4dec45aebcd242850626a747d9ba28355
hong <ghdrjsgml@gmail.com>
2016-06-27 21:36:49 +0900

    init heroku

--- public/resourcesmonthly/monthly.js
+++ public/resourcesmonthly/monthly.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
 		
 			for(var i=0;i<data.length;i++){
 				if(data[i].recommend<11){
+					console.log(data[i].recommend)
 					$('#field').append(
 							
 					'<div style="float:left;border-top:1px solid;border-bottom:1px solid;border-left:1px solid;border-right:1px solid; background-color:#FAED7D;width:150px;height:100px">'
@@ -19,6 +20,8 @@ $(document).ready(function(){
 					)
 				}
 				if(data[i].recommend<21 && data[i].recommend>10){
+					console.log(data[i].recommend)
+
 					$('#field').append(
 							
 					'<div style="float:left;border-top:1px solid;border-bottom:1px solid;border-left:1px solid;border-right:1px solid; background-color:#5CD1E5;width:250px;height:200px">'
@@ -26,6 +29,8 @@ $(document).ready(function(){
 					)
 				}
 				if(data[i].recommend<31&&data[i].recommend>20){
+					console.log(data[i].recommend)
+
 					$('#field').append(
 							
 					'<div style="float:left;border-top:1px solid;border-bottom:1px solid;border-left:1px solid;border-right:1px solid; background-color:#A566FF;width:350px;height:300px">'
@@ -33,6 +38,8 @@ $(document).ready(function(){
 					)
 				}
 				if(data[i].recommend>30){
+					console.log(data[i].recommend)
+
 					$('#field').append(
 							
 			'<div style="float:left;border-top:1px solid;border-bottom:1px solid;border-left:1px solid;border-right:1px solid; background-color:#F15F5F;width:450px;height:400px">'
