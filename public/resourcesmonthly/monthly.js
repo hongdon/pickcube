8061c1030cdbaffa2fccb529a143eb955f06e4ac
hong <ghdrjsgml@gmail.com>
2016-06-27 21:42:46 +0900

    init heroku

--- public/resourcesmonthly/monthly.js
+++ public/resourcesmonthly/monthly.js
@@ -16,7 +16,8 @@ $(document).ready(function(){
 					$('#field').append(
 							
 					'<div style="float:left;border-top:1px solid;border-bottom:1px solid;border-left:1px solid;border-right:1px solid; background-color:#FAED7D;width:150px;height:100px">'
-
+					+'<a href=/searchbyid/?id="'+data[i]._id+'">'+data[i].title+
+					'<p>'+data[i].nickname
 					)
 				}
 				if(data[i].recommend<21 && data[i].recommend>10){
@@ -25,7 +26,8 @@ $(document).ready(function(){
 					$('#field').append(
 							
 					'<div style="float:left;border-top:1px solid;border-bottom:1px solid;border-left:1px solid;border-right:1px solid; background-color:#5CD1E5;width:250px;height:200px">'
-
+							+'<a href=/searchbyid/?id="'+data[i]._id+'">'+data[i].title+
+							'<p>'+data[i].nickname			
 					)
 				}
 				if(data[i].recommend<31&&data[i].recommend>20){
@@ -34,7 +36,8 @@ $(document).ready(function(){
 					$('#field').append(
 							
 					'<div style="float:left;border-top:1px solid;border-bottom:1px solid;border-left:1px solid;border-right:1px solid; background-color:#A566FF;width:350px;height:300px">'
-
+							+'<a href=/searchbyid/?id="'+data[i]._id+'">'+data[i].title+
+							'<p>'+data[i].nickname
 					)
 				}
 				if(data[i].recommend>30){
@@ -43,7 +46,8 @@ $(document).ready(function(){
 					$('#field').append(
 							
 			'<div style="float:left;border-top:1px solid;border-bottom:1px solid;border-left:1px solid;border-right:1px solid; background-color:#F15F5F;width:450px;height:400px">'
-
+							+'<a href=/searchbyid/?id="'+data[i]._id+'">'+data[i].title+
+							'<p>'+data[i].nickname
 					)
 				}
 			}
